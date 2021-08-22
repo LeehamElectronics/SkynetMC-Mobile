0dc61cd21179703b1f381eaf37f69414ff109736
LeehamElectronics <liamdanielprice@gmail.com>
2021-08-22 10:37:11 +1000

    Initial Commit

---
+++ SkynetMC Mobile/SkynetMC_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkynetMC_MobileApp.swift
+//  SkynetMC Mobile
+//
+//  Created by Liam Price on 22/8/21.
+//
+
+import SwiftUI
+
+@main
+struct SkynetMC_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
